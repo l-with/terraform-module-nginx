052f31c08d66162a6035e9b511ba537ac2f2d4a9
Lars With <lars@with.de>
2023-02-05 10:03:47 +0100

    use index for system link

--- http_conf.tf
+++ http_conf.tf
@@ -22,5 +22,5 @@ resource "system_file" "sites_available_nginx_server_FQDN_http_conf" {
 resource "system_link" "sites_enabled_nginx_server_FQDN_http_conf" {
   count      = var.nginx_config ? 1 : 0
   path   = "${var.nginx_configuration_home}/sites-enabled/${var.nginx_server_FQDN}_http.conf"
-  target = system_file.sites_available_nginx_server_FQDN_http_conf.path
+  target = system_file.sites_available_nginx_server_FQDN_http_conf[0].path
 }
